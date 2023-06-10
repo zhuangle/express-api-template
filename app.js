b3b5a37ef7a1f3719eca5357a68d9897d4e5077b
zhuang <coderzhuang@163.com>
2023-06-10 12:49:39 +0800

    问题处理中间件的使用

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = require('./router')
 const { PORT,APIPATH } = require('./config/config.default')
+const errorHandler = require('./middleware/errorHandler.js')
 
 const app = express()
 
@@ -9,6 +10,9 @@ const app = express()
 app.use(express.json())
 
 app.use(APIPATH ,router)
+
+// 统一问题处理中间件
+app.use(errorHandler())
 
 app.listen(PORT, () => {
   console.log(`Server is Running at http://localhost:${PORT}`);
